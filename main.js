35daaf0fec6e47abbf199a4b1d42cadcf48e909b
Aziz Segaoui <Aziz@Azizs-MacBook-Pro-2.local>
2016-12-23 11:04:49 +0000

    Correction de quelque information

--- main.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var fs = require('fs');
-var dir = '~/Aziz/Desktop/Test';
-if (!fs.existsSync(dir)){
-    fs.mkdirSync(dir);
-}
-
-var server = http.createServer();
-server.listen(8888);
